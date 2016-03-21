4586bf5e14d0acb2a9046f546f8eb00177763bf9
Andrew Smith-Mui <arsm800@gmail.com>
2016-03-21 18:27:22 -0400

    Can now successfully add a location.

---
+++ app/controllers/locations_controller.rb
@@ -0,0 +1,31 @@
+class LocationsController < ApplicationController
+
+  def index
+    @locations = Location.all
+  end
+
+  def show
+    @locations = Location.find(params[:id])
+  end
+
+  def new
+    @locations = Location.new
+  end
+
+  def create
+    @location = Location.create(location_params)
+    redirect_to locations_path(@location)
+  end
+
+  def update
+    @location = Location.find(params[:id])
+    @location.update(location_params)
+    redirect_to locations_path(@location)
+  end
+
+  private
+  def location_params
+    params.require(:location).permit(:country, :territory, :territory_type)
+  end
+
+end
