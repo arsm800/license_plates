11e8290d0ef9624abc3f76629ece8f54a0771c6f
Andrew Smith-Mui <arsm800@gmail.com>
2016-03-21 15:50:07 -0400

    Created form page for new plates.  Started working on UPDATE functionality.

--- app/controllers/plates_controller.rb
+++ app/controllers/plates_controller.rb
@@ -14,4 +14,17 @@ class PlatesController < ApplicationController
     redirect_to @plate
   end
 
+  def new
+    @plate = Plate.new
+  end
+
+  def edit
+    @plate = Plate.find(params[:id])
+  end
+
+  def update
+    @plate = Plate.find(params[:id])
+    @plate.update(plate_params)
+  end
+
 end
